681439a499046d51d1fd1fce9e0139da5afca991
JavierPs287 <your-email@example.com>
2026-06-22 19:14:21 +0200

    Footer + Modos claro y oscuro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { Navbar } from './component/navbar/navbar';
 
 @Component({
@@ -9,6 +10,34 @@ import { Navbar } from './component/navbar/navbar';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
   protected readonly title = signal('amusicalarges-fe');
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit() {
+
+  if (isPlatformBrowser(this.platformId)) {
+
+    const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const applyTheme = (dark: boolean) => {
+      if (dark) {
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
+    };
+
+    applyTheme(darkMode.matches);
+
+    darkMode.addEventListener('change', (event) => {
+      applyTheme(event.matches);
+    });
+
+  }
+}
 }
