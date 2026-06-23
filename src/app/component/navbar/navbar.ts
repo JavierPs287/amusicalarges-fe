ec30fd866c494c0ed596855b6721ac2c40fa48eb
JavierPs287 <your-email@example.com>
2026-06-23 16:06:52 +0200

    Cambio navbar movil

--- src/app/component/navbar/navbar.ts
+++ src/app/component/navbar/navbar.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
@@ -9,18 +10,53 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.css',
 })
 export class Navbar {
+
   visible = true;
   lastScroll = 0;
 
+  isMobile = false;
+  menuOpen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe([
+        Breakpoints.Handset,
+        Breakpoints.TabletPortrait
+      ])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+
+        if (!this.isMobile) {
+          this.menuOpen = false;
+        }
+      });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  
   @HostListener('window:scroll')
   onScroll() {
+
     const current = window.scrollY;
 
+
     if (current > this.lastScroll && current > 60) {
-      this.visible = false; // bajando → ocultar
+
+      this.visible = false;
+
+      // cerrar menú al bajar
+      this.menuOpen = false;
+
     } else {
-      this.visible = true;  // subiendo → mostrar
+
+      this.visible = true;
+
     }
+
 
     this.lastScroll = current;
   }
