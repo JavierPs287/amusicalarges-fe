bc64ced84e0fba3f586664e6735bc057c31f3820
JavierPs287 <your-email@example.com>
2026-07-10 17:21:16 +0200

    FIX v1.0.2 Fix rutas home

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -1,8 +1,11 @@
 import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
